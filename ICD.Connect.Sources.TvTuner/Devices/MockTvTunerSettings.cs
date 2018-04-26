0f301f9cde43c7d3add8942a2944ae530fac6474
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-26 16:09:02 -0400

    refactor: Tidying

--- ICD.Connect.Sources.TvTuner/Devices/MockTvTunerSettings.cs
+++ ICD.Connect.Sources.TvTuner/Devices/MockTvTunerSettings.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using ICD.Connect.Settings.Attributes;
+﻿using ICD.Connect.Settings.Attributes;
 
 namespace ICD.Connect.Sources.TvTuner.Devices
 {
-	[KrangSettings(FACTORY_NAME)]
+	[KrangSettings("MockTvTuner", typeof(MockTvTunerDevice))]
 	public sealed class MockTvTunerSettings : AbstractTvTunerSettings
 	{
-		private const string FACTORY_NAME = "MockTvTuner";
-
-		/// <summary>
-		/// Gets the originator factory name.
-		/// </summary>
-		public override string FactoryName { get { return FACTORY_NAME; } }
-
-		/// <summary>
-		/// Gets the type of the originator for this settings instance.
-		/// </summary>
-		public override Type OriginatorType { get { return typeof(MockTvTunerDevice); } }
 	}
 }
