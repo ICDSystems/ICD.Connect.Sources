fa366a283fecbe986693e64d230976bcc000028b
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-16 13:43:05 -0400

    refactor: Tidying

--- ICD.Connect.Sources.BarcoPro/BarcoClickshareProDeviceSettings.cs
+++ ICD.Connect.Sources.BarcoPro/BarcoClickshareProDeviceSettings.cs
@@ -1,22 +1,10 @@
-﻿using System;
-using ICD.Connect.Settings.Attributes;
+﻿using ICD.Connect.Settings.Attributes;
 using ICD.Connect.Sources.Barco;
 
 namespace ICD.Connect.Sources.BarcoPro
 {
-	[KrangSettings(FACTORY_NAME)]
+	[KrangSettings("BarcoClickshare", typeof(BarcoClickshareProDevice))]
 	public sealed class BarcoClickshareProDeviceSettings : AbstractBarcoClickshareDeviceSettings
 	{
-		private const string FACTORY_NAME = "BarcoClickshare";
-
-		/// <summary>
-		/// Gets the originator factory name.
-		/// </summary>
-		public override string FactoryName { get { return FACTORY_NAME; } }
-
-		/// <summary>
-		/// Gets the type of the originator for this settings instance.
-		/// </summary>
-		public override Type OriginatorType { get { return typeof(BarcoClickshareProDevice); } }
 	}
 }
