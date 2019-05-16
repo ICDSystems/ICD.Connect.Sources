61dcddc1e4913258bddb551065488ed55fc10a41
Chris Cameron <chris.cameron@wedoresi.com>
2019-05-16 12:10:20 -0400

    chore: Updating changelog, incrementing patch version

--- ICD.Connect.Sources.BarcoPro/Properties/AssemblyInfo.cs
+++ ICD.Connect.Sources.BarcoPro/Properties/AssemblyInfo.cs
@@ -3,6 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Sources.BarcoPro")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Sources.BarcoPro")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("5.1.1.0")]
-
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyVersion("5.1.2.0")]
--- ICD.Connect.Sources.TvTuner/Properties/AssemblyInfo.cs
+++ ICD.Connect.Sources.TvTuner/Properties/AssemblyInfo.cs
@@ -3,6 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Sources.TvTuner")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Sources.TvTuner")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("5.1.1.0")]
-
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyVersion("5.1.2.0")]
