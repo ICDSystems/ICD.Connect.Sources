cf87bb029c21d5bfd1a572571bbef7c8aabe558f
Laura Gomez <laura.gomez@profound-tech.com>
2019-05-30 14:45:26 -0400

    feat: added the Search method

--- ICD.Connect.Sources.Roku/RokuDevice.cs
+++ ICD.Connect.Sources.Roku/RokuDevice.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using ICD.Common.Properties;
 using ICD.Common.Utils;
 using ICD.Common.Utils.Services.Logging;
@@ -9,151 +9,152 @@ using ICD.Connect.Protocol.Extensions;
 using ICD.Connect.Protocol.Network.Ports.Web;
 using ICD.Connect.Protocol.Network.Settings;
 using ICD.Connect.Settings;
+using System.Collections.Generic;
+using ICD.Common.Utils.Extensions;
 
 namespace ICD.Connect.Sources.Roku
 {
-    public sealed class RokuDevice : AbstractDevice<RokuDeviceSettings>
-    {
-        private readonly UriProperties m_UriProperties;
-        private IWebPort m_Port;
+	public sealed class RokuDevice : AbstractDevice<RokuDeviceSettings>
+	{
+		private readonly UriProperties m_UriProperties;
+		private IWebPort m_Port;
 
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        public RokuDevice()
-        {
-            m_UriProperties = new UriProperties();
-        }
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public RokuDevice()
+		{
+			m_UriProperties = new UriProperties();
+		}
 
-        /// <summary>
-        /// Sets the port for communication with the service.
-        /// </summary>
-        /// <param name="port"></param>
-        [PublicAPI]
-        public void SetPort(IWebPort port)
-        {
-            if (port == m_Port)
-                return;
+		/// <summary>
+		/// Sets the port for communication with the service.
+		/// </summary>
+		/// <param name="port"></param>
+		[PublicAPI]
+		public void SetPort(IWebPort port)
+		{
+			if (port == m_Port)
+				return;
 
-            ConfigurePort(port);
+			ConfigurePort(port);
 
-            Unsubscribe(m_Port);
+			Unsubscribe(m_Port);
 
-            if (port != null)
-                port.Accept = "application/xml";
+			if (port != null)
+				port.Accept = "application/xml";
 
-            m_Port = port;
-            Subscribe(m_Port);
+			m_Port = port;
+			Subscribe(m_Port);
 
-            UpdateCachedOnlineStatus();
-        }
-
-
-        /// <summary>
-        /// Configures the given port for communication with the device.
-        /// </summary>
-        /// <param name="port"></param>
-        private void ConfigurePort(IWebPort port)
-        {
-            // URI
-            if (port != null)
-                port.ApplyDeviceConfiguration(m_UriProperties);
-        }
-
-        protected override bool GetIsOnlineStatus()
-        {
-            return m_Port != null && m_Port.IsOnline;
-        }
-
-        /// <summary>
-        /// Subscribe to the port events.
-        /// </summary>
-        /// <param name="port"></param>
-        private void Subscribe(IWebPort port)
-        {
-            if (port == null)
-                return;
-
-            port.OnIsOnlineStateChanged += PortOnIsOnlineStateChanged;
-        }
-
-        /// <summary>
-        /// Unsubscribe from the port events.
-        /// </summary>
-        /// <param name="port"></param>
-        private void Unsubscribe(IWebPort port)
-        {
-            if (port == null)
-                return;
-
-            port.OnIsOnlineStateChanged -= PortOnIsOnlineStateChanged;
-        }
-
-        private void PortOnIsOnlineStateChanged(object sender, DeviceBaseOnlineStateApiEventArgs e)
-        {
-            UpdateCachedOnlineStatus();
-        }
-
-        #region Settings
-
-        /// <summary>
-        /// Override to apply settings to the instance.
-        /// </summary>
-        /// <param name="settings"></param>
-        /// <param name="factory"></param>
-        protected override void ApplySettingsFinal(RokuDeviceSettings settings, IDeviceFactory factory)
-        {
-            base.ApplySettingsFinal(settings, factory);
-
-            m_UriProperties.Copy(settings);
-
-            IWebPort port = null;
-
-            if (settings.Port != null)
-            {
-                try
-                {
-                    port = factory.GetPortById((int)settings.Port) as IWebPort;
-                }
-                catch (KeyNotFoundException)
-                {
-                    Log(eSeverity.Error, "No web port with id {0}", settings.Port);
-                }
-            }
-
-            SetPort(port);
-        }
-
-        /// <summary>
-        /// Override to clear the instance settings.
-        /// </summary>
-        protected override void ClearSettingsFinal()
-        {
-            base.ClearSettingsFinal();
-
-            SetPort(null);
-
-            m_UriProperties.Clear();
-        }
-
-        /// <summary>
-        /// Override to apply properties to the settings instance.
-        /// </summary>
-        /// <param name="settings"></param>
-        protected override void CopySettingsFinal(RokuDeviceSettings settings)
-        {
-            base.CopySettingsFinal(settings);
-
-            settings.Port = m_Port == null ? (int?)null : m_Port.Id;
-
-            settings.Copy(m_UriProperties);
-        }
+			UpdateCachedOnlineStatus();
+		}
 
 
+		/// <summary>
+		/// Configures the given port for communication with the device.
+		/// </summary>
+		/// <param name="port"></param>
+		private void ConfigurePort(IWebPort port)
+		{
+			// URI
+			if (port != null)
+				port.ApplyDeviceConfiguration(m_UriProperties);
+		}
+
+		protected override bool GetIsOnlineStatus()
+		{
+			return m_Port != null && m_Port.IsOnline;
+		}
+
+		/// <summary>
+		/// Subscribe to the port events.
+		/// </summary>
+		/// <param name="port"></param>
+		private void Subscribe(IWebPort port)
+		{
+			if (port == null)
+				return;
+
+			port.OnIsOnlineStateChanged += PortOnIsOnlineStateChanged;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the port events.
+		/// </summary>
+		/// <param name="port"></param>
+		private void Unsubscribe(IWebPort port)
+		{
+			if (port == null)
+				return;
+
+			port.OnIsOnlineStateChanged -= PortOnIsOnlineStateChanged;
+		}
+
+		private void PortOnIsOnlineStateChanged(object sender, DeviceBaseOnlineStateApiEventArgs e)
+		{
+			UpdateCachedOnlineStatus();
+		}
+
+		#region Settings
+
+		/// <summary>
+		/// Override to apply settings to the instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		/// <param name="factory"></param>
+		protected override void ApplySettingsFinal(RokuDeviceSettings settings, IDeviceFactory factory)
+		{
+			base.ApplySettingsFinal(settings, factory);
+
+			m_UriProperties.Copy(settings);
+
+			IWebPort port = null;
+
+			if (settings.Port != null)
+			{
+				try
+				{
+					port = factory.GetPortById((int) settings.Port) as IWebPort;
+				}
+				catch (KeyNotFoundException)
+				{
+					Log(eSeverity.Error, "No web port with id {0}", settings.Port);
+				}
+			}
+
+			SetPort(port);
+		}
+
+		/// <summary>
+		/// Override to clear the instance settings.
+		/// </summary>
+		protected override void ClearSettingsFinal()
+		{
+			base.ClearSettingsFinal();
+
+			SetPort(null);
+
+			m_UriProperties.Clear();
+		}
+
+		/// <summary>
+		/// Override to apply properties to the settings instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		protected override void CopySettingsFinal(RokuDeviceSettings settings)
+		{
+			base.CopySettingsFinal(settings);
+
+			settings.Port = m_Port == null ? (int?) null : m_Port.Id;
+
+			settings.Copy(m_UriProperties);
+		}
 
 		#endregion
-		
+
 		#region Keypress Methods
+
 		public enum eRokuKeys
 		{
 			Home,
@@ -177,63 +178,114 @@ namespace ICD.Connect.Sources.Roku
 		/// Sends an HTTP POST command to press and release a specific key
 		/// </summary>
 		/// <param name="key"></param>
-		private void Keypress(eRokuKeys key)
+		public void Keypress(eRokuKeys key)
 		{
-			string unused;
-			m_Port.Post(string.Format("/keypress/{0}",key.ToString()), new byte[0], out unused);
+			Post(string.Format("/keypress/{0}", key.ToString()));
 		}
+
 		/// <summary>
 		/// Sends an HTTP POST command to press and hold a specific key
 		/// </summary>
 		/// <param name="key"></param>
-		private void Keydown(eRokuKeys key)
+		public void Keydown(eRokuKeys key)
 		{
-			string unused;
-			m_Port.Post(string.Format("/keydown/{0}", key.ToString()), new byte[0], out unused);
+			Post(string.Format("/keydown/{0}", key.ToString()));
 		}
+
 		/// <summary>
 		/// Sends an HTTP POST command to release a specific key
 		/// </summary>
 		/// <param name="key"></param>
-		private void Keyup(eRokuKeys key)
+		public void Keyup(eRokuKeys key)
 		{
-			string unused;
-			m_Port.Post(string.Format("/keyup/{0}", key.ToString()), new byte[0], out unused);
-		}
-		#endregion
-
-		#region Post Methods
-
-		private void LaunchApp(int appID)
-		{
-			string unused;
-			m_Port.Post(string.Format("/launch/{0}", appID.ToString()), new byte[0], out unused);
+			Post(string.Format("/keyup/{0}", key.ToString()));
 		}
 
-	    private void InstallApp(int channelID)
-	    {
-		    string unused;
-		    m_Port.Post(string.Format("/install/{0}", channelID.ToString()), new byte[0], out unused);
-	    }
 		#endregion
+
+
+		public void LaunchApp(int appId)
+		{
+			Post(string.Format("/launch/{0}", appId.ToString()));
+		}
+
+		public void InstallApp(int channelId)
+		{
+			Post(string.Format("/install/{0}", channelId.ToString()));
+		}
+
+		#region Search
+
+		public void RokuSearch(IEnumerable<KeyValuePair<eRokuSearchPar, string>> parameters)
+		{
+			string query = new RokuSearchQueryBuilder().Append(parameters).ToString();
+			Post(string.Format("/search/browse{0}", query));
+		}
+
+		private void Post(string path)
+		{
+			path = Uri.EscapeUriString(path);
+
+			string unused;
+			m_Port.Post(path, new byte[0], out unused);
+		}
+
+		#endregion
+
+		#region Console
+
 		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
 		{
-			foreach (IConsoleCommand command in base.GetConsoleCommands())
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
 				yield return command;
 
 			string keyHelp =
 				string.Format("Keys: <{0}>", StringUtils.ArrayFormat(EnumUtils.GetValues<eRokuKeys>()));
+			string searchHelp =
+				("search Parameters: <[Keyword, Title]>");
+			string multiSearchHelp =
+				string.Format("Search Parameters: <{0}>", StringUtils.ArrayFormat(EnumUtils.GetValues<eRokuSearchPar>()));
 
 			yield return new GenericConsoleCommand<eRokuKeys>("Keypress", "Press and release key. " + keyHelp, k => Keypress(k));
 			yield return new GenericConsoleCommand<eRokuKeys>("Keydown", "Press and hold key. " + keyHelp, k => Keydown(k));
 			yield return new GenericConsoleCommand<eRokuKeys>("Keyup", "Release key. " + keyHelp, k => Keyup(k));
-			yield return new GenericConsoleCommand<int>("Launch", "Launches the channel identified by appID",T =>LaunchApp(T));
-			yield return new GenericConsoleCommand<int>("Install", "Exits the current channel, and launches the Channel Store details screen of the channel identified by appID", T => InstallApp(T));
+			yield return new GenericConsoleCommand<int>("Launch", "Launches the channel identified by appID", T => LaunchApp(T));
+			yield return new GenericConsoleCommand<int>("Install",
+				"Exits the current channel, and launches the Channel Store details screen of the channel identified by appID",
+				T => InstallApp(T));
+			yield return new GenericConsoleCommand<eRokuSearchPar, string>("Search",
+				"Searches with the parameter and your term. " + searchHelp, (k, t) => RokuSearch(k, t));
+			yield return
+				new GenericConsoleCommand<eRokuSearchPar, string, eRokuSearchPar, string>(
+					"MultiSearch", "Searches with two parameters, and 1 term per parameter (keyword or title must be a parameter). " + multiSearchHelp, (k, t, h, q) => RokuSearch(k, t, h, q));
 		}
 
-	    private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
 		{
 			return base.GetConsoleCommands();
 		}
-    }
+
+		private void RokuSearch(eRokuSearchPar par, string term)
+		{
+			KeyValuePair<eRokuSearchPar, string>[] query =
+			{
+				new KeyValuePair<eRokuSearchPar, string>(par, term)
+			};
+
+			RokuSearch(query);
+		}
+
+		private void RokuSearch(eRokuSearchPar par, string term, eRokuSearchPar secondPar, string secondTerm)
+		{
+			KeyValuePair<eRokuSearchPar, string>[] query =
+			{
+				new KeyValuePair<eRokuSearchPar, string>(par, term),
+				new KeyValuePair<eRokuSearchPar, string>(secondPar, secondTerm)
+			};
+
+			RokuSearch(query);
+		}
+
+		#endregion
+	}
 }
---
+++ ICD.Connect.Sources.Roku/RokuSearchQueryBuilder.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ICD.Common.Utils.Collections;
+
+namespace ICD.Connect.Sources.Roku
+{
+	public sealed class UriQueryBuilder
+	{
+		private readonly Dictionary<string, string> m_Parameters;
+
+		public UriQueryBuilder()
+		{
+			m_Parameters = new Dictionary<string, string>();
+		}
+
+		public UriQueryBuilder Append(string key, string value)
+		{
+			m_Parameters.Add(key, value);
+			return this;
+		}
+
+		public override string ToString()
+		{
+			StringBuilder builder = new StringBuilder("?");
+
+			bool first = true;
+
+			foreach (KeyValuePair<string, string> kvp in m_Parameters)
+			{
+				if (!first)
+					builder.Append('&');
+				first = false;
+
+				builder.Append(kvp.Key);
+				builder.Append('=');
+				builder.Append(kvp.Value);
+			}
+
+			return builder.ToString();
+		}
+	}
+
+	public enum eRokuSearchPar
+	{
+		Keyword,
+		Title,
+		Type,
+		Tmsid,
+		Season,
+		Showunavailable,
+		MatchAny,
+		ProviderId,
+		Provider,
+		Launch
+	}
+
+	public enum eRokuSearchType
+	{
+		Movie,
+		TvShow,
+		Person,
+		Channel,
+		Game
+	}
+
+	public class RokuSearchQueryBuilder
+	{
+		private static readonly Dictionary<eRokuSearchPar, string> s_SearchPars =
+			new Dictionary<eRokuSearchPar, string>
+			{
+				{eRokuSearchPar.Keyword, "keyword"},
+				{eRokuSearchPar.Title, "title"},
+				{eRokuSearchPar.Type, "type"},
+				{eRokuSearchPar.Tmsid, "tmsid"},
+				{eRokuSearchPar.Season, "season"},
+				{eRokuSearchPar.Showunavailable, "show-unavailable"},
+				{eRokuSearchPar.MatchAny, "match-any"},
+				{eRokuSearchPar.ProviderId, "provider-id"},
+				{eRokuSearchPar.Provider, "provider"},
+				{eRokuSearchPar.Launch, "launch"}
+			};
+
+		private static readonly IcdHashSet<string> s_SearchTypes =
+			new IcdHashSet<string>
+			{
+				"movie",
+				"tv-show",
+				"person",
+				"channel",
+				"game"
+			};
+
+		private readonly UriQueryBuilder m_Builder;
+
+		public RokuSearchQueryBuilder()
+		{
+			m_Builder = new UriQueryBuilder();
+		}
+
+		public RokuSearchQueryBuilder Append(eRokuSearchPar parameter, string value)
+		{
+			if (parameter == eRokuSearchPar.Type && !s_SearchTypes.Contains(value))
+				throw new ArgumentException("Unexpected type value", "value");
+
+			string param = s_SearchPars[parameter];
+			m_Builder.Append(param, value);
+			return this;
+		}
+
+		public RokuSearchQueryBuilder Append(IEnumerable<KeyValuePair<eRokuSearchPar, string>> parameters)
+		{
+			if (parameters == null)
+				throw new ArgumentNullException("parameters");
+
+			foreach (KeyValuePair<eRokuSearchPar, string> kvp in parameters)
+				Append(kvp.Key, kvp.Value);
+
+			return this;
+		}
+
+		public override string ToString()
+		{
+			return m_Builder.ToString();
+		}
+	}
+}
