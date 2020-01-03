8e47360d28a060821d471b52abaf2d089345af65
Chris Cameron <chris.cameron@wedoresi.com>
2020-01-03 10:28:32 -0500

    chore: Updating AssemblyInfo

--- ICD.Connect.Sources.Roku/Properties/AssemblyInfo.cs
+++ ICD.Connect.Sources.Roku/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("ICD.Connect.Sources.Roku")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Sources.Roku")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
 [assembly: AssemblyVersion("6.3.0.0")]
 
